34e541efe34fb056d88ec6c0adfcc16f40702b3e
Saumya Puthenveettil <sputhen2000@gmail.com>
2024-12-25 23:04:45 -0500

    Add files via upload

---
+++ POMS/HomePage.java
@@ -0,0 +1,86 @@
+package com.revature.poms;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class HomePage {
+
+    private WebDriver driver;
+
+    @FindBy(id = "greeting")
+    private WebElement greetingHeader;
+
+    @FindBy(id = "logoutButton")
+    private WebElement logoutButton;
+
+    @FindBy(tagName = "tr")
+    private List<WebElement> tableRows;
+
+    @FindBy(id = "locationSelect")    //option select for moon/planet
+    private WebElement optionValue;
+
+    @FindBy(className= "submit-button")
+    private WebElement submitButton;
+
+    @FindBy(id = "planetNameInput")
+    private WebElement planetNameInput;
+
+    @FindBy(tagName="td")
+    private List<WebElement> tableCols;
+
+    @FindBy(tagName = "celestialTable")
+    private List<WebElement> celestialTable;
+
+
+
+    public HomePage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public String getHomePageGreeting(){
+        return greetingHeader.getText();
+    }
+
+    public int getNumberOfCelestialRows(){
+        return tableRows.size()-1;
+    }
+
+    public void tryToAccessHomePageDirectly(){
+        driver.get("http://localhost:8080/planetarium");
+    }
+
+    public void logout(){
+        logoutButton.click();
+    }
+
+    public String getOptionSelectedValue(){
+        return optionValue.getText();
+    }
+
+    public void submit(){
+        submitButton.click();
+    }
+
+    public void enterPlanetName(String planetName){
+        planetNameInput.sendKeys(planetName);
+    }
+
+
+    public Integer getCelestialTableRowsSize(){
+        return tableRows.size();
+
+    }
+
+    //public List<WebElement> getCelestialCols(List <WebElement> tableRows){
+     //   tableCols = tableRows.get(0).findElements(By.tagName("td"));
+        //tableCols = tableRows.findElements(By.tagName("td"));
+      //  return tableCols;
+   // }
+}
---
+++ POMS/LoginPage.java
@@ -0,0 +1,44 @@
+package com.revature.poms;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    private WebDriver driver;
+
+    @FindBy(id = "usernameInput")
+    private WebElement usernameInput;
+
+    @FindBy(id = "passwordInput")
+    private WebElement passwordInput;
+
+    @FindBy(tagName = "input")
+    private WebElement loginButton;
+
+    @FindBy(tagName = "a")
+    private WebElement registrationLink;
+
+    public LoginPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void setUpLoggedInUser(){
+        driver.get("http://localhost:8080/");
+        usernameInput.sendKeys("Batman");
+        passwordInput.sendKeys("Iamthenight1939");
+        loginButton.submit();
+    }
+
+    public void openLoginPage(){
+        driver.get("http://localhost:8080/");
+    }
+
+    public void clickRegistrationLink(){
+        registrationLink.click();
+    }
+
+}
